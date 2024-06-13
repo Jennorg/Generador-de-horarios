58cd01efe203ebe4a003e4a0933b2a3bbf58c8dd
Jenorg <jenfermz44@gmail.com>
2024-06-13 09:42:34 -0400

    feat: :tada: Extraccion de datos del excel

---
+++ src/app.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+archivo = "DATOSHORARIOS.xlsx"
+
+profesores = pd.read_excel(archivo, sheet_name= "Profesores")
+materias = pd.read_excel(archivo, sheet_name= "Materias")
+aulas = pd.read_excel(archivo, sheet_name= "Aulas")
+modalidades = pd.read_excel(archivo, sheet_name="Modalidades")
+carreras = pd.read_excel(archivo, sheet_name= "Carreras")
+bloques = pd.read_excel(archivo, sheet_name= "Bloques")
+restricciones = pd.read_excel(archivo, sheet_name= "Restricciones")
+
+modalidades_alta_prioridad = modalidades[modalidades["Prioridades"] == "Alta"]
+aulas_del_atlantico = aulas[aulas["Sede "] == "Atlántico "]
+
+print(aulas_del_atlantico)
